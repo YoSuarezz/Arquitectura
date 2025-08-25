b2e18c2e75eb4b4ef80c6faf0bcc839fe300f335
TuNombre <tuemail@example.com>
2025-08-24 23:34:11 -0500

    arreglo

--- Laboratorio 1.asm
+++ Laboratorio 1.asm
@@ -26,6 +26,8 @@
     ingresar_f: .asciiz "\nIngrese el valor de f: "
     resultado_distancia: .asciiz "\nLa distancia es: "
     resultado_velocidad: .asciiz "\nLa velocidad es: "
+    resultado_aceleracion: .asciiz "\nLa aceleracion angular es: "
+    resultado_velocidad_final: .asciiz "\nLa velocidad angular final es: "
 
 .text
 .globl main
@@ -62,19 +64,6 @@ opcion_movimientos:
     beq $t1, 2, submenu_mcua
     jr $ra
 
-submenu_mru:
-    la $a0, menu_mru
-    li $v0, 4
-    syscall
-
-    li $v0, 5
-    syscall
-    move $t2, $v0
-
-    beq $t2, 1, mru_distancia
-    beq $t2, 2, mru_velocidad
-    jr $ra
-
 mru_distancia:
     # Pedir velocidad
     la $a0, ingresar_velocidad
@@ -93,18 +82,17 @@ mru_distancia:
     mov.s $f2, $f0  # guardar tiempo
 
     # Calcular distancia = v * t
-    mul.s $f3, $f1, $f2
+    mul.s $f12, $f1, $f2
 
     # Imprimir resultado
     la $a0, resultado_distancia
     li $v0, 4
     syscall
 
-    mov.s $f12, $f3
     li $v0, 2       # print float
     syscall
     
-    j loop_main   # volver al menu principal
+    jr $ra  # volver al menu principal
 
 mru_velocidad:
     # Pedir distancia
@@ -124,18 +112,40 @@ mru_velocidad:
     mov.s $f2, $f0  # guardar tiempo
 
     # Calcular velocidad = d / t
-    div.s $f3, $f1, $f2
+    div.s $f12, $f1, $f2
 
     # Imprimir resultado
     la $a0, resultado_velocidad
     li $v0, 4
     syscall
 
-    mov.s $f12, $f3
     li $v0, 2       # print float
     syscall
     
-    j loop_main   # volver al menu principal
+    jr $ra   # volver al menu principal
+
+submenu_mru:
+    la $a0, menu_mru
+    li $v0, 4
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t2, $v0
+
+    beq $t2, 1, ejecutar_mru_dist
+    beq $t2, 2, ejecutar_mru_vel
+    jr $ra
+
+ejecutar_mru_dist:
+    jal mru_distancia
+    j loop_main
+
+ejecutar_mru_vel:
+    jal mru_velocidad
+    j loop_main
+
+
 
 submenu_mcua:
     la $a0, menu_mcua
@@ -146,9 +156,17 @@ submenu_mcua:
     syscall
     move $t3, $v0
 
-    beq $t3, 1, mcua_aceleracion
-    beq $t3, 2, mcua_velocidad_final
+    beq $t3, 1, ejecutar_mcua_aceleracion
+    beq $t3, 2, ejecutar_mcua_velfinal
     jr $ra
+
+ejecutar_mcua_aceleracion:
+    jal mcua_aceleracion
+    j loop_main
+
+ejecutar_mcua_velfinal:
+    jal mcua_velocidad_final
+    j loop_main
 
 mcua_aceleracion:
     # Pedir velocidad angular final
@@ -177,18 +195,16 @@ mcua_aceleracion:
 
     # Calculo: alpha = (w_f - w_0) / t
     sub.s $f4, $f1, $f2
-    div.s $f5, $f4, $f3
+    div.s $f12, $f4, $f3
 
     # Imprimir resultado
-    la $a0, ingresar_aceleracion
+    la $a0,  resultado_aceleracion
     li $v0, 4
     syscall
-
-    mov.s $f12, $f5
     li $v0, 2
     syscall
 
-    j loop_main
+    jr $ra
 
 
 mcua_velocidad_final:
@@ -218,18 +234,19 @@ mcua_velocidad_final:
 
     # Calculo: w_f = w_0 + alpha * t
     mul.s $f4, $f2, $f3
-    add.s $f5, $f1, $f4
+    add.s $f12, $f1, $f4
 
     # Imprimir resultado
-    la $a0, ingresar_velocidad_final
+    la $a0, resultado_velocidad_final
     li $v0, 4
     syscall
 
-    mov.s $f12, $f5
     li $v0, 2
     syscall
 
-    j loop_main
+    jr $ra
+
+
 
 # --------- TRIGONOMETRICA --------------
 opcion_trigonometrica:
